84d86b4ab2d27df783b7f852d4961734f4de5040
tokio220506.pavel@gmail.com <tokio220506.pavel@gmail.com>
2024-03-20 19:24:07 +0300

    .

--- Project1/logic.cpp
+++ Project1/logic.cpp
@@ -1,6 +1,9 @@
 
 
 int the_arithmetic_mean_of_the_nonzero_elements_of_the_vector(int* vector, int size) {
+	if (size <= 0) {
+		return -1;
+	}
 	int arithmetic = 0;
 	int count = 0;
 
--- Project3/logic.cpp
+++ Project3/logic.cpp
@@ -3,6 +3,9 @@
 
 
 bool logic(int* vector, int size) {
+	if (size <= 0) {
+		return false;
+	}
 
 	for (int i = 0; i < size; i++) {
 		for (int j = i + 1; j < size; j++) {
